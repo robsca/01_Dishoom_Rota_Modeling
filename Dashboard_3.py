977a1c04bd4b8211fd2fb8a871ba0318fed3b7fc
robsca <scalas.roberto@gmail.com>
2022-09-23 16:53:04 +0100

    Add selection feature for all_restaurant - download csv feature added

--- Dashboard_3.py
+++ Dashboard_3.py
@@ -11,41 +11,60 @@ with st.sidebar.expander('Import data'):
     uploaded_file_2 = st.file_uploader("Select Covers Data")
 
 if uploaded_file_1 is not None and uploaded_file_2 is not None:
-
     data_employee = create_final_timeseries(uploaded_file_1,uploaded_file_2)
-    
-    with st.expander('Data'):
-        st.write(data_employee)
 
-    with st.sidebar.expander('Download data'):
+    with st.expander('guestsVSlabour - Data'):
+        st.write(data_employee)
         @st.cache # cache the function
         def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv().encode('utf-8')
         
         csv = convert_df(data_employee)
-        name = st.text_input('File name', 'data')
+        # get start_date
+        start_date = data_employee.index.min().strftime('%Y-%m-%d')
+        # get end_date
+        end_date = data_employee.index.max().strftime('%Y-%m-%d')
+        name_ = f'guestsVSlabour_{start_date}_{end_date}'
+        name = st.text_input('File name', name_)
         st.download_button(
             label="Download data as CSV",
             data=csv,
             file_name=f'{name}.csv',
             mime='text/csv',
         )
-        
+
     # create a list of restaurants
     restaurants = data_employee['Store_Name'].unique()
+    # add all restaurants to the list
+    import numpy as np
+    
+    restaurants = np.append(restaurants, 'All Restaurants')
 
-    selected = st.selectbox('Select a restaurant', restaurants)
+    selected = st.sidebar.selectbox('Select a restaurant', restaurants)
+    if selected == 'All Restaurants':
+        # drop the column 'Store_Name'
+        data_employee = data_employee.drop(columns=['Store_Name'])
+        # group by hour
+        data_employee = data_employee.groupby(data_employee.index).sum()
+        # create a new column 'Hours'
+        data_employee['Hour'] = data_employee.index.hour
+        # add a column 'Day'
+        data_employee['Day_of_week'] = data_employee.index.day_name()
+        #st.write(data_employee)
+        st.subheader('Employees Data - All Restaurants')
     # get the df for that restaurant
-    data_employee = data_employee[data_employee['Store_Name'] == selected]
-
+    else:
+        data_employee = data_employee[data_employee['Store_Name'] == selected]
+        st.subheader(f'Employees Data - {selected}')
+        #st.write(data_employee)
     # ----------------- #
     # plot
     import plotly.graph_objects as go
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data_employee.index, y=data_employee['Guest_Count'], name='Guest Count'))
-    fig.add_trace(go.Scatter(x=data_employee.index, y=data_employee['Employees_Count'], name='Employees'))
-    fig.update_layout(
+    fig_timeries = go.Figure()
+    fig_timeries.add_trace(go.Scatter(x=data_employee.index, y=data_employee['Guest_Count'], name='Guest Count', fill='tozeroy'))
+    fig_timeries.add_trace(go.Scatter(x=data_employee.index, y=data_employee['Employees_Count'], name='Employees Count', fill='tozeroy'))
+    fig_timeries.update_layout(
         title="Guest Count vs Employees",
         xaxis_title="Date",
         yaxis_title="Count",
@@ -55,4 +74,84 @@ if uploaded_file_1 is not None and uploaded_file_2 is not None:
             color="#7f7f7f"
         )
     )
+
+    # ----------------- #
+    # CREATE HEATMAP
+    # Filter out hours < 9 and > 23
+    data_employee = data_employee[(data_employee.index.hour >= 9) & (data_employee.index.hour < 23)]
+    
+    # group by day making average of the guests and employees
+    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    frame = []
+    for day in days_of_week:
+        # filter by day
+        data_employee_day = data_employee[data_employee['Day_of_week'] == day]
+        data_employee_day = data_employee_day.groupby(data_employee['Hour']).mean()
+        # modify guest count as ratio
+        data_employee_day['Guest_Count'] = data_employee_day['Guest_Count'] / data_employee_day['Employees_Count']        
+        # drop hour column
+        data_employee_day = data_employee_day.drop(columns=['Hour'])
+        # drop employees count
+        data_employee_day = data_employee_day.drop(columns=['Employees_Count'])
+        # rename guest count
+        data_employee_day = data_employee_day.rename(columns={'Guest_Count': day})
+        transposed_day = data_employee_day.T
+        # add to list
+        frame.append(transposed_day)
+
+    data_employees_heat = pd.concat(frame)
+    with st.expander('Heatmap_data'):
+        st.write(data_employees_heat)
+    
+    st.plotly_chart(fig_timeries, use_container_width=True)
+
+    # plot heatmap
+    import plotly.express as px
+    fig = px.imshow(data_employees_heat, labels=dict(x="Hour", y="Day of week", color="Guests per employee"))
+    # from red to green
+    
+
+    fig.update_layout(
+        title="Guests per employee",
+        xaxis_title="Hour",
+        yaxis_title="Day of week",
+        legend_title="Legend Title",
+        font=dict(
+            family="Courier New, monospace",
+            color="#7f7f7f"
+        )
+    )
+    #st.plotly_chart(fig, use_container_width=True)
+
+    # plot as go
+    import plotly.graph_objects as go
+    fig = go.Figure(data=go.Heatmap(    
+        z=data_employees_heat.values,
+        x=data_employees_heat.columns,
+        y=data_employees_heat.index[::-1],
+        colorscale='Turbo',
+        # set as min of the whole dataset
+        zmin=data_employees_heat.values.min(),
+        # set as max of the whole df
+        zmax=data_employees_heat.values.max(),
+        colorbar=dict(
+            title="Guests per employee",
+            titleside="right",
+            tickmode="array",
+            tickvals=[data_employees_heat.values.min(), 2.0, data_employees_heat.values.max()],
+            ticktext=["Over-Staffed", "Ideal", "Under-Staffed"],
+            ticks="outside"
+        )
+    ))
+    fig.update_layout(
+        title="Guests per employee",
+        xaxis_title="Hour",
+        yaxis_title="Day of week",
+        legend_title="Legend Title",
+        font=dict(
+            family="Courier New, monospace",
+            color="#7f7f7f"
+        )
+    )
     st.plotly_chart(fig, use_container_width=True)
+
